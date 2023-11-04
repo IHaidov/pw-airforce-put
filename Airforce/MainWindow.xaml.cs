1fd9f6ddfec7b3f754f38937ae1d56d552666122
IHaidov <vanya.haidov@gmail.com>
2023-11-04 18:07:42 +0100

    Main window aircraft initial design components and events adds.

--- Airforce/MainWindow.xaml.cs
+++ Airforce/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BL = Alesik.Haidov.Airforce.BLC;
 
 namespace Alesik.Haidov.Airforce.UI
 {
@@ -19,9 +20,59 @@ namespace Alesik.Haidov.Airforce.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ViewModels.AirbaseListViewModel AirbaseLVM { get; } = new ViewModels.AirbaseListViewModel();
+        private ViewModels.AirbaseViewModel selectedAirbase = null;
+
+        public ViewModels.AircraftListViewModel AircraftLVM { get; } = new ViewModels.AircraftListViewModel();
+        private ViewModels.AircraftViewModel selectedAircraft = null;
+
+        private readonly BL.BLC blc;
+
+        private string selectedDataSource = "AirforceDBMock.dll";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AircraftList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ApplyAircraftSearch(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ApplyAircraftModelFilter(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ApplyAircraftServiceHoursFilter(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ApplyNewDataSource(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditAircraft(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveAircraft(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddAircraft(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }

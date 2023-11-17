39d2c00a20676bfb76257ff3b7911a1a5f1970a4
IHaidov <vanya.haidov@gmail.com>
2023-11-17 16:34:09 +0100

    DB File implementation adds (aircrafts.bin, airbases.bin) + Base SQL implementation.

---
+++ Airforce.DBSQL/AirbaseDBSQL.cs
@@ -0,0 +1,29 @@
+﻿using Alesik.Haidov.Airforce.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alesik.Haidov.Airforce.DBSQL
+{
+    internal class AirbaseDBSQL
+    {
+        [Key]
+        public string GUID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public IAirbase ToIAirbase()
+        {
+            return new Airbase() { GUID = GUID, Name = Name, Location = Location};
+        }
+    }
+    class Airbase : IAirbase
+    {
+        public string GUID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ Airforce.DBSQL/AircraftDBSQL.cs
@@ -0,0 +1,32 @@
+﻿using Alesik.Haidov.Airforce.Core;
+using Alesik.Haidov.Airforce.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alesik.Haidov.Airforce.DBSQL
+{
+    internal class AircraftDBSQL
+    {
+        public string GUID { get; set; }
+        public string Model { get; set; }
+        public int ServiceHours { get; set; }
+        public AircraftType Type { get; set; }
+        public string AirbaseGUID { get; set; }
+
+        public IAircraft ToIAircraft(List<AirbaseDBSQL> airbases)
+        {
+            return new Aircraft() { GUID = GUID, Model = Model, ServiceHours = ServiceHours, Type = Type, Airbase = airbases.Single(airbase => airbase.GUID.Equals(AirbaseGUID)).ToIAirbase()};
+        }
+    }
+    class Aircraft : IAircraft
+    {
+        public string GUID { get; set; }
+        public string Model { get; set; }
+        public int ServiceHours { get; set; }
+        public AircraftType Type { get; set; }
+        public IAirbase Airbase { get; set; }
+    }
+}

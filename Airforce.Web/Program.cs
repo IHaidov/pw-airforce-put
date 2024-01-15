df10ee1fde2373a6036372559cf6c7ec916f1394
IHaidov <vanya.haidov@gmail.com>
2024-01-16 00:28:50 +0100

    Fixing DI.

---
+++ Airforce.Web/Program.cs
@@ -0,0 +1,52 @@
+using Alesik.Haidov.Airforce.Web.Services;
+using Alesik.Haidov.Airforce.BLC;
+using Alesik.Haidov.Airforce.Interfaces;
+using Alesik.Haidov.Airforce.DBSQL;
+
+namespace Alesik.Haidov.Airforce.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<DAOSQL>();
+            var dataSource = builder.Configuration.GetValue<string>("ConnectionStrings:DataSource");
+
+            builder.Services.AddSingleton<BLC.BLC>();
+            builder.Services.AddScoped<AirbaseService>(provider => new AirbaseService(provider.GetRequiredService<BLC.BLC>(), dataSource));
+            builder.Services.AddScoped<AircraftService>(provider => new AircraftService(provider.GetRequiredService<BLC.BLC>(), dataSource));
+            
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
